1edf3f1fd65f6787b0f6aa80eec6250ac86e72b3
Samwaa <kuo77122@gmail.com>
2016-01-11 17:55:11 +0800

    build views 未完成

--- SmartHome/api/views.py
+++ SmartHome/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from .models import House, Nodes, NodeState, CurrentState, IRcommend
+from .serializers import HouseSerializer, NodesSerializer
+
+class JSONResponse(HttpResponse):
+    # """
+    # An HttpResponse that renders its content into JSON.
+    # """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+# 未完成
+@csrf_exempt
+def House_Setting(request):
+    if request.method == 'GET':
+        HouseSetting = House.objects.all()
+        serializer = HouseSerializer(HouseSetting, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = HouseSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+# class HouseViewSet(viewsets.ModelViewSet):
+#     queryset = House.objects.all()
+#     serializer_class = HouseSerializer
+
+
+# class NodesViewSet(viewsets.ModelViewSet):
+#     queryset = Nodesobjects.all()
+#     serializer_class = NodesSerializer
